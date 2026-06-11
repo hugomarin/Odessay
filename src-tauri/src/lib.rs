05374c1debcc0147c98bfc99e8504d410709efbc
Hugo <web.hugomarin@gmail.com>
2026-06-11 08:33:19 -0600

    fix(desktop): add tauri-plugin-process crate and init for relaunch() [ODE-224]

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
